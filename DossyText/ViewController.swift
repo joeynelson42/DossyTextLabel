781127dd6ca80d5c075f0355c6a350471b5e05b6
joey <joeynelson42@gmail.com>
2017-07-09 08:09:20 -0600

    Initial Commit

---
+++ DossyText/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DossyText
+//
+//  Created by Joey Nelson on 7/9/17.
+//  Copyright © 2017 NelsonJE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
